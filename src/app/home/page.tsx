1799e1297a579c2bfc0a539ee74ec6fd1cacdddd
kallol000 <kallolhazarika33@gmail.com>
2025-09-08 23:31:26 +0530

    fixed the quote issue

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -125,7 +125,7 @@ export default function HomePage() {
         return (
             <div className="px-4 h-full">
                 <Card className="text-xl h-full flex items-center justify-center p-4">
-                    <CardTitle>Let's set up your profile</CardTitle>
+                    <CardTitle>{"Let's set up your profile"}</CardTitle>
                     <NewUserSetupPopover setHomeRefresh = {setHomeRefresh} />
                 </Card>
             </div>
